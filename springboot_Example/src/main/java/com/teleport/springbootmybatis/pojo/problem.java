aba8f4e787e1b88f102b4d89c10786307528b144
teleport-10032 <58069438+teleport-10032@users.noreply.github.com>
2020-10-13 00:55:14 +0800

    update

---
+++ springboot_Example/src/main/java/com/teleport/springbootmybatis/pojo/problem.java
@@ -0,0 +1,15 @@
+package com.teleport.springbootmybatis.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class problem {
+
+    private Integer id;
+    private String title;
+
+}
